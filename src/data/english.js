d2085992b5f76b12e2c11be36bf1ac22986db359
lukelee1983 <75567505@qq.com>
2026-05-26 20:58:30 +0800

    Add src/data/english.js

---
+++ src/data/english.js
@@ -0,0 +1,75 @@
+/**
+ * 二年级上册英语知识点库（人教版新起点一起点）
+ * 数据来源：百度网盘《二年级英语上册知识点汇总（人教版一起点）》
+ */
+export default {
+  subject: '英语',
+  grade: '二年级上册',
+  chapters: [
+    {
+      name: 'Unit 1: Family（家庭）',
+      topics: [
+        { name: '家庭成员词汇', keywords: ['family', 'father', 'mother', 'brother', 'sister', 'grandfather', 'grandmother', 'dad', 'mum', '爸爸', '妈妈', '哥哥', '姐姐', '爷爷', '奶奶', '家庭'] },
+        { name: '性别表达', keywords: ['boy', 'girl', 'he', 'she', '男孩', '女孩', '他', '她'] },
+        { name: '介绍家庭句型', keywords: ['This is my', 'This is my family', 'He is a boy', 'She is a girl', '这是我的', '他是一个', '她是一个'] },
+        { name: 'Who句型', keywords: ["Who's he", "Who's she", '他是谁', '她是谁'] }
+      ]
+    },
+    {
+      name: 'Unit 2: Boys and Girls（男孩和女孩）',
+      topics: [
+        { name: '询问名字', keywords: ["What's his name", "What's her name", 'His name is', 'Her name is', '名字', '他叫什么', '她叫什么'] },
+        { name: '描述外貌', keywords: ['What does he look like', 'What does she look like', 'tall', 'short', 'thin', 'handsome', 'pretty', 'big', 'small', '高', '矮', '瘦', '帅', '漂亮'] },
+        { name: '形容词词汇', keywords: ['tall', 'short', 'big', 'small', 'fat', 'thin', 'handsome', 'pretty', 'new', 'old'] }
+      ]
+    },
+    {
+      name: 'Unit 3: My Friends（我的朋友）',
+      topics: [
+        { name: '交友与介绍', keywords: ['friend', 'friends', 'my friend', '朋友', '我的朋友'] },
+        { name: '描述朋友', keywords: ['He/She is my friend', 'play together', '他她是我的朋友', '一起玩'] }
+      ]
+    },
+    {
+      name: 'Unit 4: In the Community（在社区）',
+      topics: [
+        { name: '地点词汇', keywords: ['park', 'zoo', 'school', 'hospital', 'library', 'bookstore', 'supermarket', '公园', '动物园', '学校', '医院', '图书馆', '书店', '超市'] },
+        { name: 'Where句型', keywords: ['Where are you going', "I'm going to the", '你要去哪里', '我要去'] },
+        { name: 'There be句型', keywords: ['There is', 'There are', '有', '某处有某物'] }
+      ]
+    },
+    {
+      name: 'Unit 5: In the Park（在公园）',
+      topics: [
+        { name: '公园事物词汇', keywords: ['tree', 'flower', 'grass', 'boat', 'lake', 'hill', 'bench', '树', '花', '草', '船', '湖', '山', '长椅'] },
+        { name: '公园活动', keywords: ['boat on the lake', 'trees in the park', '湖上有船', '公园有树'] }
+      ]
+    },
+    {
+      name: 'Unit 6: Happy Holidays（快乐的节日）',
+      topics: [
+        { name: '节日问候', keywords: ['Merry Christmas', 'Happy New Year', 'Happy birthday', '圣诞节快乐', '新年快乐', '生日快乐', 'Welcome', '欢迎'] },
+        { name: '礼物相关', keywords: ['Here is a present for you', 'present', 'gift', '这是给你的礼物', '礼物'] },
+        { name: '感受表达', keywords: ['Delicious', 'Yummy', '好吃', '美味'] }
+      ]
+    },
+    {
+      name: '字母与书写',
+      topics: [
+        { name: '26个字母', keywords: ['字母', '大小写', 'alphabet', 'A到Z'] },
+        { name: '字母书写规范', keywords: ['书写', '四线格', '大写字母', '小写字母', '书写格式', '占格'] },
+        { name: '单词拼写', keywords: ['拼写', 'spell', 'write', '填空', '补充单词'] }
+      ]
+    },
+    {
+      name: '核心句型总复习',
+      topics: [
+        { name: 'This is... 句型', keywords: ['This is', 'That is', 'This is my', '这是', '那是'] },
+        { name: 'Who... 句型', keywords: ["Who's he", "Who's she", 'Who is', '是谁'] },
+        { name: 'What... 句型', keywords: ["What's his/her name", 'What does he/she look like', 'What is', '什么'] },
+        { name: 'Where... 句型', keywords: ['Where are you going', 'Where is', '哪里', '去哪里'] },
+        { name: 'There be 句型', keywords: ['There is', 'There are', '有'] }
+      ]
+    }
+  ]
+}
